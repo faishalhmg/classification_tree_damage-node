c519e8c929e673b8a014fc7cce76cd3edbacd024
faishalhmg <faishalhmg@yahoo.co.id>
2023-08-06 23:31:07 +0700

    test

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const tf = require('@tensorflow/tfjs-node');
 const cocoSsd = require('@tensorflow-models/coco-ssd');
 const { createCanvas, loadImage } = require('canvas');
 
-
-
 const app = express();
 const PORT = 80;
+
+// Konfigurasi penyimpanan file menggunakan Multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // Gunakan middleware untuk mengurai data formulir dari permintaan POST
 app.use(express.urlencoded({ extended: true }));
@@ -28,98 +30,74 @@ tf.loadGraphModel(`file://${modelPath}`)
 
 // Fungsi deteksi objek
 async function detectObjects(imgBuffer) {
-    const inputTensor = tf.node.decodeImage(imgBuffer);
-    const inputShape = [320, 320]; // Ganti dengan bentuk input model Anda
-    const inputArray = inputTensor.toFloat().div(255);
-    const inputBatch = inputArray.expandDims();
-  
-    // Convert the dtype of the input tensor to int32
-    const inputBatchInt32 = inputBatch.toInt();
-  
-    const detections = await model.executeAsync({ 'input_tensor': inputBatchInt32 });
-    const numDetections = detections[3].dataSync()[0]; // Use dataSync() to get scalar value
-    const detectionBoxes = detections[0].dataSync().slice(0, numDetections * 4); // Flatten the tensor
-    const detectionScores = detections[1].dataSync().slice(0, numDetections);
-    const detectionClasses = detections[2].dataSync().slice(0, numDetections);
-  
-    const detectedObjects = [];
-    for (let i = 0; i < numDetections; i++) {
-      const classIndex = detectionClasses[i];
-      const className = class_names[classIndex];
-      const score = detectionScores[i];
-      const [ymin, xmin, ymax, xmax] = detectionBoxes.slice(i * 4, (i + 1) * 4);
-  
-      detectedObjects.push({
-        class: className,
-        score: score,
-        boundingBox: { ymin, xmin, ymax, xmax }
-      });
-    }
-  
-    return detectedObjects;
+  const inputTensor = tf.node.decodeImage(imgBuffer);
+  const inputShape = [320, 320]; // Ganti dengan bentuk input model Anda
+  const inputArray = inputTensor.toFloat().div(255);
+  const inputBatch = inputArray.expandDims();
+
+  // Convert the dtype of the input tensor to int32
+  const inputBatchInt32 = inputBatch.toInt();
+
+  const detections = await model.executeAsync({ 'input_tensor': inputBatchInt32 });
+  const numDetections = detections[3].dataSync()[0]; // Use dataSync() to get scalar value
+  const detectionBoxes = detections[0].dataSync().slice(0, numDetections * 4); // Flatten the tensor
+  const detectionScores = detections[1].dataSync().slice(0, numDetections);
+  const detectionClasses = detections[2].dataSync().slice(0, numDetections);
+
+  const detectedObjects = [];
+  for (let i = 0; i < numDetections; i++) {
+    const classIndex = detectionClasses[i];
+    const className = class_names[classIndex];
+    const score = detectionScores[i];
+    const [ymin, xmin, ymax, xmax] = detectionBoxes.slice(i * 4, (i + 1) * 4);
+
+    detectedObjects.push({
+      class: className,
+      score: score,
+      boundingBox: { ymin, xmin, ymax, xmax }
+    });
   }
 
+  return detectedObjects;
+}
+
 // Konstanta class_names dan class_colors yang Anda gunakan sebelumnya
-const class_names = ["akar_Patah-mati",
-"batang-akar_patah",
-"batang_pecah",
-"brum akar atau batang",
-"cabang patah mati",
-"daun berubah warna",
-"daun pucuk tunas rusak",
-"gerowong",
-"hilang pucuk dominan",
-"kanker",
-"konk",
-"liana",
-"luka terbuka",
-"percabangan brum berlebihan",
-"resinosis gumosis",
-"sarang rayap"];
-
-// const class_colors = [
-//   [255, 0, 0],
-//   [0, 255, 0],
-//   [0, 0, 255],
-//   [255, 255, 0],
-//   [0, 255, 255],
-//   [255, 0, 255],
-//   [128, 0, 0],
-//   [0, 128, 0],
-//   [0, 0, 128],
-//   [128, 128, 0],
-//   [0, 128, 128],
-//   [128, 0, 128],
-//   [255, 128, 0],
-//   [128, 255, 0],
-//   [0, 128, 255],
-//   [255, 0, 128],
-// ];
-
-
-// Konfigurasi penyimpanan file menggunakan Multer
-// Konfigurasi penyimpanan file menggunakan Multer
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const class_names = [
+  "akar_Patah-mati",
+  "batang-akar_patah",
+  "batang_pecah",
+  "brum akar atau batang",
+  "cabang patah mati",
+  "daun berubah warna",
+  "daun pucuk tunas rusak",
+  "gerowong",
+  "hilang pucuk dominan",
+  "kanker",
+  "konk",
+  "liana",
+  "luka terbuka",
+  "percabangan brum berlebihan",
+  "resinosis gumosis",
+  "sarang rayap"
+];
 
 // Endpoint untuk deteksi objek pada gambar
 app.post('/detect', upload.single('file'), async (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'Tidak ada file yang diunggah.' });
-    }
-  
-    const imgBuffer = req.file.buffer;
-  
-    try {
-      const detectedObjects = await detectObjects(imgBuffer);
-  
-      // Draw bounding boxes on image
-      drawBoundingBoxes(imgBuffer, detectedObjects, res);
-    } catch (err) {
-      console.error('Gagal melakukan deteksi objek:', err);
-      res.status(500).json({ error: 'Terjadi kesalahan saat melakukan deteksi objek.' });
-    }
-  });
+  if (!req.file) {
+    return res.status(400).json({ error: 'Tidak ada file yang diunggah.' });
+  }
+
+  const imgBuffer = req.file.buffer;
+
+  try {
+    const detectedObjects = await detectObjects(imgBuffer);
+
+    res.json({ objects: detectedObjects });
+  } catch (err) {
+    console.error('Gagal melakukan deteksi objek:', err);
+    res.status(500).json({ error: 'Terjadi kesalahan saat melakukan deteksi objek.' });
+  }
+});
 
 // Fungsi untuk menggambar bounding boxes pada gambar
 async function drawBoundingBoxes(imgBuffer, detectedObjects) {
@@ -155,7 +133,6 @@ function getRandomColor() {
 }
 
 
-  
 
 // Jalankan server
 app.listen(PORT, () => {
